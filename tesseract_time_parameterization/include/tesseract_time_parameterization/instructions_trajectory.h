6907026937ed60b0344e5844ddb97e21c959fab3
John Wason <wason@wasontech.com>
2021-07-20 16:27:13 -0400

    Add SWIG shared_ptr to trajectory containers

--- tesseract_time_parameterization/include/tesseract_time_parameterization/instructions_trajectory.h
+++ tesseract_time_parameterization/include/tesseract_time_parameterization/instructions_trajectory.h
@@ -35,6 +35,10 @@ TESSERACT_COMMON_IGNORE_WARNINGS_POP
 #include <tesseract_command_language/core/instruction.h>
 #include <tesseract_command_language/composite_instruction.h>
 
+#ifdef SWIG
+%shared_ptr(tesseract_planning::InstructionsTrajectory)
+#endif
+
 namespace tesseract_planning
 {
 class InstructionsTrajectory : public TrajectoryContainer
