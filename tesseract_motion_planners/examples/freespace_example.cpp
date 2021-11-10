f102da004cfc3e86bd054f001a5fa062466739dd
Chen Bainian <chenbn@artc.a-star.edu.sg>
2021-11-10 14:53:20 +0800

    Fix example opw_kinematics include build error
    Signed-off-by: Chen Bainian <chenbn@artc.a-star.edu.sg>

--- tesseract_motion_planners/examples/freespace_example.cpp
+++ tesseract_motion_planners/examples/freespace_example.cpp
@@ -26,12 +26,10 @@
 #include <tesseract_common/macros.h>
 TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
 #include <console_bridge/console.h>
-#include <opw_kinematics/opw_parameters.h>
 TESSERACT_COMMON_IGNORE_WARNINGS_POP
 
 #include <tesseract_common/types.h>
 
-#include <tesseract_kinematics/opw/opw_inv_kin.h>
 #include <tesseract_kinematics/core/utils.h>
 
 #include <tesseract_environment/environment.h>
