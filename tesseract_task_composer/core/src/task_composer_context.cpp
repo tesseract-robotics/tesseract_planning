1b148264d2b9419e5e7bc30bc68275e31eac0c5d
Levi Armstrong <levi.armstrong@gmail.com>
2024-04-11 14:48:04 -0500

    fixup

--- tesseract_task_composer/core/src/task_composer_context.cpp
+++ tesseract_task_composer/core/src/task_composer_context.cpp
@@ -26,6 +26,7 @@
 
 #include <tesseract_common/macros.h>
 TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
+#include <boost/version.hpp>
 #if (BOOST_VERSION >= 107400) && (BOOST_VERSION < 107500)
 #include <boost/serialization/library_version_type.hpp>
 #endif
