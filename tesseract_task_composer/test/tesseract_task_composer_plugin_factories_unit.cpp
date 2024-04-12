8766b979b1edf84b8edcb1f863b37bfa844351cc
Levi Armstrong <levi.armstrong@gmail.com>
2024-04-12 09:07:05 -0500

    fixup

--- tesseract_task_composer/test/tesseract_task_composer_plugin_factories_unit.cpp
+++ tesseract_task_composer/test/tesseract_task_composer_plugin_factories_unit.cpp
@@ -51,7 +51,7 @@ void runTaskComposerFactoryTest(TaskComposerPluginFactory& factory, YAML::Node p
 
     for (auto it = search_paths.begin(); it != search_paths.end(); ++it)
     {
-      EXPECT_TRUE(std::find(sp.begin(), sp.end(), it->as<std::string>()) != sp.end());
+      EXPECT_TRUE(sp.find(it->as<std::string>()));
     }
   }
 
@@ -61,7 +61,7 @@ void runTaskComposerFactoryTest(TaskComposerPluginFactory& factory, YAML::Node p
 
     for (auto it = search_libraries.begin(); it != search_libraries.end(); ++it)
     {
-      EXPECT_TRUE(std::find(sl.begin(), sl.end(), it->as<std::string>()) != sl.end());
+      EXPECT_TRUE(sl.find(it->as<std::string>()));
     }
   }
 
