2e5da4df5bcb3f1e000f06a92474a337a3486582
Levi Armstrong <levi.armstrong@gmail.com>
2025-07-05 21:02:41 -0500

    Add OMPL collision cost objective

---
+++ tesseract_motion_planners/ompl/include/tesseract_motion_planners/ompl/collision_cost_objective.h
@@ -0,0 +1,100 @@
+/**
+ * @file collision_cost_objective.h
+ * @brief Tesseract OMPL planner collision cost objective
+ *
+ * @author Levi Armstrong
+ * @date July 5, 2025
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @copyright Copyright (c) 2025, Levi Armstrong
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef TESSERACT_MOTION_PLANNERS_COLLISION_COST_OBJECTIVE_H
+#define TESSERACT_MOTION_PLANNERS_COLLISION_COST_OBJECTIVE_H
+
+#include <tesseract_common/macros.h>
+TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
+#include <ompl/base/objectives/StateCostIntegralObjective.h>
+#include <ompl/base/SpaceInformation.h>
+#include <mutex>
+#include <memory>
+#include <map>
+#include <vector>
+#include <string>
+TESSERACT_COMMON_IGNORE_WARNINGS_POP
+
+#include <tesseract_motion_planners/ompl/types.h>
+
+#include <tesseract_environment/fwd.h>
+#include <tesseract_kinematics/core/fwd.h>
+#include <tesseract_collision/core/fwd.h>
+
+namespace tesseract_planning
+{
+/**
+ * @class PenetrationDepthObjective
+ * @brief Optimization objective for minimizing the obstacles within the contact threshold.
+ *
+ * This objective assigns zero cost to states outside contact threshold and a cost equal to the distance within the
+ * contact threshold (positive) for states inside obstacles. When used with StateCostIntegralObjective
+ * (optimize_by_motion=true), the planner minimizes the integral of penetration along the path.
+ */
+class CollisionCostObjective : public ompl::base::StateCostIntegralObjective
+{
+public:
+  CollisionCostObjective(const ompl::base::SpaceInformationPtr& space_info,
+                         const tesseract_environment::Environment& env,
+                         std::shared_ptr<const tesseract_kinematics::JointGroup> manip,
+                         const tesseract_collision::ContactManagerConfig& contact_manager_config,
+                         OMPLStateExtractor extractor,
+                         bool optimize_by_motion = false);
+
+  /**
+   * @brief State cost function.
+   * @param state The state to evaluate.
+   * @return Cost equal to the distance within the contact threshold at state s (>=0).
+   */
+  ompl::base::Cost stateCost(const ompl::base::State* state) const override;
+
+private:
+  /** @brief The Tesseract Joint Group */
+  std::shared_ptr<const tesseract_kinematics::JointGroup> manip_;
+
+  /** @brief The continuous contact manager used for creating cached continuous contact managers. */
+  std::shared_ptr<tesseract_collision::DiscreteContactManager> contact_manager_;
+
+  /** @brief A list of active links */
+  std::vector<std::string> links_;
+
+  /** @brief This will extract an Eigen::VectorXd from the OMPL State */
+  OMPLStateExtractor extractor_;
+
+  // The items below are to cache the contact manager based on thread ID. Currently ompl is multi
+  // threaded but the methods used to implement collision checking are not thread safe. To prevent
+  // reconstructing the collision environment for every check this will cache a contact manager
+  // based on its thread ID.
+
+  /** @brief Contact manager caching mutex */
+  mutable std::mutex mutex_;
+
+  /** @brief The continuous contact manager cache */
+  mutable std::map<unsigned long int, std::shared_ptr<tesseract_collision::DiscreteContactManager>> contact_managers_;
+};
+
+}  // namespace tesseract_planning
+
+#endif  // TESSERACT_MOTION_PLANNERS_COLLISION_COST_OBJECTIVE_H
---
+++ tesseract_motion_planners/ompl/src/collision_cost_objective.cpp
@@ -0,0 +1,100 @@
+/**
+ * @file collision_cost_objective.cpp
+ * @brief Tesseract OMPL planner collision cost objective
+ *
+ * @author Levi Armstrong
+ * @date July 5, 2025
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @copyright Copyright (c) 2025, Levi Armstrong
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <tesseract_common/macros.h>
+TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
+#include <ompl/base/SpaceInformation.h>
+#include <thread>
+#include <Eigen/Core>
+TESSERACT_COMMON_IGNORE_WARNINGS_POP
+
+#include <tesseract_motion_planners/ompl/utils.h>
+#include <tesseract_motion_planners/ompl/collision_cost_objective.h>
+
+#include <tesseract_kinematics/core/joint_group.h>
+#include <tesseract_collision/core/discrete_contact_manager.h>
+#include <tesseract_collision/core/types.h>
+#include <tesseract_environment/environment.h>
+
+namespace tesseract_planning
+{
+CollisionCostObjective::CollisionCostObjective(const ompl::base::SpaceInformationPtr& space_info,
+                                               const tesseract_environment::Environment& env,
+                                               std::shared_ptr<const tesseract_kinematics::JointGroup> manip,
+                                               const tesseract_collision::ContactManagerConfig& contact_manager_config,
+                                               OMPLStateExtractor extractor,
+                                               bool optimize_by_motion)
+  : StateCostIntegralObjective(space_info, optimize_by_motion)
+  , manip_(std::move(manip))
+  , contact_manager_(env.getDiscreteContactManager())
+  , extractor_(std::move(extractor))
+{
+  links_ = manip_->getActiveLinkNames();
+
+  contact_manager_->setActiveCollisionObjects(links_);
+  contact_manager_->applyContactManagerConfig(contact_manager_config);
+}
+
+ompl::base::Cost CollisionCostObjective::stateCost(const ompl::base::State* state) const
+{
+  // It was time using chronos time elapsed and it was faster to cache the contact manager
+  unsigned long int hash = std::hash<std::thread::id>{}(std::this_thread::get_id());
+  tesseract_collision::DiscreteContactManager::Ptr cm;
+  mutex_.lock();
+  auto it = contact_managers_.find(hash);
+  if (it == contact_managers_.end())
+  {
+    cm = contact_manager_->clone();
+    contact_managers_[hash] = cm;
+  }
+  else
+  {
+    cm = it->second;
+  }
+  mutex_.unlock();
+
+  Eigen::Map<Eigen::VectorXd> finish_joints = extractor_(state);
+  tesseract_common::TransformMap state1 = manip_->calcFwdKin(finish_joints);
+
+  for (const auto& link_name : links_)
+    cm->setCollisionObjectsTransform(link_name, state1[link_name]);
+
+  tesseract_collision::ContactResultMap contact_map;
+  cm->contactTest(contact_map, tesseract_collision::ContactTestType::CLOSEST);
+
+  if (contact_map.empty())
+    return ompl::base::Cost(0.0);
+
+  tesseract_collision::ContactResultVector contact_vector;
+  contact_map.flattenMoveResults(contact_vector);
+
+  // penalty = how far you're *inside* (0 outside)
+  double penetration{ 0.0 };
+  for (const auto& cr : contact_vector)
+    penetration = std::max(-cr.distance, penetration);
+
+  return ompl::base::Cost(penetration);
+}
+}  // namespace tesseract_planning
